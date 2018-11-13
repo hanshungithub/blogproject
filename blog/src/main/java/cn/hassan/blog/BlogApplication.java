42c4a171acf7a98ba55210f5faf6a9a2c11a7f78
hassan <hassanbox@qq.com>
2018-11-13 22:00:58 +0800

    Initial commit

---
+++ blog/src/main/java/cn/hassan/blog/BlogApplication.java
@@ -0,0 +1,14 @@
+package cn.hassan.blog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("cn.hassan.**.mapper")
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
